389d02ebaeb7d9435f56b72a840aabc3fb9e6863
Stefania Holguin Q <stefania.holguin@nivelics.co>
2022-02-18 16:31:53 -0500

    Estilos más leidos mobile y desktop

--- src/app/cards-notices/cards-notices.component.ts
+++ src/app/cards-notices/cards-notices.component.ts
@@ -23,6 +23,22 @@ export class CardsNoticesComponent implements OnInit {
       'text1':'Alaín Castillo',
       'text2':'.',
       'text3':'Nov 28, 2021',
+    },
+    {
+      'imageCard':'../../../../assets/images/banners/covid.png',
+      'title':'¿Cómo los robots mexicanos están apoyando ante el COVID-19?',
+      'parrafo':'Hay empresas en el país que están desarrollando soluciones innovadoras que no hubiesen sido ...',
+      'text1':'Alaín Castillo',
+      'text2':'.',
+      'text3':'Nov 28, 2021',
+    },
+    {
+      'imageCard':'../../../../assets/images/banners/industria.png',
+      'title':'La industria de la manufactura aditiva en México',
+      'parrafo':'El sector sigue experimentando un crecimiento saludable.',
+      'text1':'Alaín Castillo',
+      'text2':'.',
+      'text3':'Nov 28, 2021',
     }
   ]
 
